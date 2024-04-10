13790729d6e276fd360d9af03bf4173f7a74e19d
marcin-jedrysik <m.marcin.2014@o2.pl>
2024-04-10 23:46:53 +0200

    Dodaj pliki projektów.

---
+++ Pogoda/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Pogoda.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+#pragma warning disable CA1822 // Mark members as static
+        public string Greeting => "Witam";
+#pragma warning restore CA1822 // Mark members as static
+    }
+}
---
+++ Pogoda/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace Pogoda.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
